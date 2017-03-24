763bd0027fe8d2b3894bf09ab10575f4fbd3ba14
Allen Gong <alm.gong@gmail.com>
2017-03-24 16:45:51 -0700

    avoid using Jade templating, send back static HTML for index page

--- projects/pm-tycoon-html5/src/pm-tycoon/routes/index.js
+++ projects/pm-tycoon-html5/src/pm-tycoon/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+var relativeViewPath = '../views/'; 	// relative to __dirname
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, relativeViewPath, 'index.html'));
 });
 
 module.exports = router;
